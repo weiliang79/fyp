ee508ef7e1e5701eea02f7e4831412a5f319b72a
Wei Liang <liang2010@hotmail.my>
2022-09-04 22:58:38 +0800

    feat: add new student

--- laravel-e-canteen-management-system/app/Http/Controllers/UserManagementController.php
+++ laravel-e-canteen-management-system/app/Http/Controllers/UserManagementController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\RestTime;
 use App\Models\Role;
+use App\Models\Student;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -12,8 +14,9 @@ use function PHPSTORM_META\map;
 class UserManagementController extends Controller
 {
     public function index(){
-        $users = User::get();
-        return view('admin.user_management.index', compact('users'));
+        $users = User::all();
+        $students = Student::all();
+        return view('admin.user_management.index', compact('users', 'students'));
     }
 
     public function showCreateForm(){
@@ -27,7 +30,7 @@ class UserManagementController extends Controller
             'first_name' => 'required',
             'last_name' => 'required',
             'username' => 'required',
-            'email' => 'unique:users,email',
+            'email' => 'unique:users,email|unique:students,email',
             'password' => 'required|min:8',
             'role' => 'required|integer|gt:0'
         ],
@@ -44,12 +47,60 @@ class UserManagementController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
-        return redirect()->route('admin.user_management')->with('swal-success', 'New user detail save successful.');
+        return redirect()->route('admin.user_management')->with('swal-success', 'New user details save successful.');
 
     }
 
     public function delete(Request $request){
         User::destroy($request->user_id);
         return response()->json('User delete successful.');
+    }
+
+    public function showStudentCreateForm(){
+
+        if(count(RestTime::all()) == 0){
+            return redirect()->route('admin.user_management.student.rest_time');
+        }
+
+        $restTimes = RestTime::all();
+
+        return view('admin.user_management.student.create', compact('restTimes'));
+    }
+
+    public function saveStudent(Request $request){
+
+        $request->validate([
+            'student_id' => 'required',
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'username' => 'required',
+            'email' => 'nullable|unique:users,email|unique:students,email',
+            'password' => 'required',
+            'rest_id.*' => 'integer|gt:0',
+        ],
+        [
+            'rest_id.*.gt' => 'The rest time field need to choose a rest time.',
+        ]);
+
+        //dd($request);
+
+        $student = Student::create([
+            'student_id' => $request->student_id,
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'phone' => $request->phone,
+            'address' => $request->address,
+        ]);
+
+        if($request->rest_id){
+            foreach($request->rest_id as $id){
+                $student->restTimes()->attach($id);
+            }
+        }
+
+        return redirect()->route('admin.user_management')->with('swal-success', 'New Student Details save successful.');
     }
 }
