2b330ff659730665e769a01816e64ab4cd16654c
Wei Liang <liang2010@hotmail.my>
2022-09-04 22:57:03 +0800

    fix: change phone and address to nullable

--- laravel-e-canteen-management-system/database/migrations/2022_08_15_082409_create_students_table.php
+++ laravel-e-canteen-management-system/database/migrations/2022_08_15_082409_create_students_table.php
@@ -23,8 +23,8 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->string('phone');
-            $table->string('address');
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
