8b2a35e293076100fd9f9017c83257b0af4773c6
Wei Liang <liang2010@hotmail.my>
2022-09-04 12:57:39 +0800

    feat: add flatpickr

--- laravel-e-canteen-management-system/resources/js/app.js
+++ laravel-e-canteen-management-system/resources/js/app.js
@@ -13,6 +13,10 @@ DataTable(window, window.$);
 import Swal from 'sweetalert2';
 window.Swal = Swal;
 
+// flatpickr
+import flatpickr from 'flatpickr';
+window.flatpickr = flatpickr;
+
 window.addEventListener('DOMContentLoaded', event => {
 
       const sidebarToggle = document.body.querySelector('#sidebarToggle');
@@ -31,6 +35,12 @@ window.addEventListener('DOMContentLoaded', event => {
                         next: '<i class="fa-solid fa-angle-right"></i>',
                   }
             }
+      });
+
+      flatpickr('.timepicker', {
+            enableTime: true,
+            noCalendar: true,
+            dateFormat: 'h:i K',
       });
 
 })
