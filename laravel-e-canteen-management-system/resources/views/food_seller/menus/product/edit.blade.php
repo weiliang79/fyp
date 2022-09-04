8640e625c3d380b331424b40ac9e565e5687eea4
Wei Liang <liang2010@hotmail.my>
2022-09-04 13:01:11 +0800

    feat: add form error notification

--- laravel-e-canteen-management-system/resources/views/food_seller/menus/product/edit.blade.php
+++ laravel-e-canteen-management-system/resources/views/food_seller/menus/product/edit.blade.php
@@ -12,6 +12,14 @@
 
                         <div class="card-body">
 
+                              @if($errors->any())
+                              <div class="row my-4">
+                                    <div class="col-md-8 offset-md-3 text-danger">
+                                          <i class="fa-solid fa-circle-exclamation fa-lg"></i> The form has some error, please refill and submit again.
+                                    </div>
+                              </div>
+                              @endif
+
                               <form action="{{ Illuminate\Support\Str::startsWith(Route::currentRouteName(), 'food_seller.menus.product.create') ? route('food_seller.menus.product.save') : route('food_seller.menus.product.update') }}" method="post">
                                     @csrf
                                     @include('food_seller.menus.product._form', ['product' => $product, 'categories' => $categories])
@@ -23,7 +31,7 @@
                                                 </button>
                                           </div>
                                     </div>
-                                    
+
                               </form>
 
                         </div>
