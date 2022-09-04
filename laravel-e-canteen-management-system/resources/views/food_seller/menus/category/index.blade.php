cc8944fe49962454af579dc0c0b304741e4bfefe
Wei Liang <liang2010@hotmail.my>
2022-09-04 12:59:42 +0800

    fix: card style not match others

--- laravel-e-canteen-management-system/resources/views/food_seller/menus/category/index.blade.php
+++ laravel-e-canteen-management-system/resources/views/food_seller/menus/category/index.blade.php
@@ -4,7 +4,7 @@
 
 <div class="container py-4">
       <div class="row justify-content-center">
-            <div class="col-md-10">
+            <div class="col-md-20">
                   <div class="card">
                         <div class="card-header">
                               Product Category
