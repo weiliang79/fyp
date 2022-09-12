b97c14b84139a0fa937cf4e16c1225a65ea29722
Wei Liang <liang2010@hotmail.my>
2022-09-12 22:00:05 +0800

    fix: not mentioned foreign key

--- laravel-e-canteen-management-system/app/Models/ProductCategory.php
+++ laravel-e-canteen-management-system/app/Models/ProductCategory.php
@@ -21,6 +21,6 @@ class ProductCategory extends Model
     ];
 
     public function products(){
-        return $this->hasMany(Product::class);
+        return $this->hasMany(Product::class, 'category_id');
     }
 }
